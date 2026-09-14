13cf4ca6d28775ba24ec4cd34aedf506f4bf36b7
Swapna-5724 <swapnakoranimath@gmail.com>
2026-09-14 23:28:24 +0530

    Add files via upload

---
+++ Lecture _ 4/Dictionary.py
@@ -0,0 +1,21 @@
+info = {
+    "key" : "value",
+    "name" : "apnacollege"
+    "learning" : "coding"
+}
+
+print(info)
+
+
+#  02
+
+info = {
+    "name" : "apnacollege"
+    "subjects" : [c"python", "C", "Java"],
+    "topics" : (),
+    "age" : 35,
+    "is_adult" : True,
+    "marks" : 94.4
+}
+
+print(info)
