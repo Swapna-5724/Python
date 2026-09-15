c8871d1ea0fead6539f10807630b9f339d1f4fd8
Swapna-5724 <swapnakoranimath@gmail.com>
2026-09-15 14:35:56 +0530

    Add files via upload

--- Lecture _ 4/Dictionary.py
+++ Lecture _ 4/Dictionary.py
@@ -1,6 +1,6 @@
 info = {
     "key" : "value",
-    "name" : "apnacollege"
+    "name" : "apnacollege",
     "learning" : "coding"
 }
 
@@ -10,12 +10,30 @@ print(info)
 #  02
 
 info = {
-    "name" : "apnacollege"
-    "subjects" : [c"python", "C", "Java"],
+    "name" : "apnacollege",
+    "subjects" : ["c", "python", "C", "Java"],
     "topics" : (),
     "age" : 35,
     "is_adult" : True,
     "marks" : 94.4
-}
+    # 12.99 : 94.4
+    }
 
 print(info)
+
+print(type(info))
+
+print(info["name"])
+print(info["topics"])
+print(info["subjects"])
+print(info["age"])
+
+
+print(info["surname"])   # It will come error
+
+
+#   To change 'name'
+
+info["name"] = "shradhakhapra"
+print(info)
+
